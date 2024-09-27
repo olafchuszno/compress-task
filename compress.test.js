b60338587cf15ce2d620448041d53717450648eb
Olaf <olaf.chuszno@gmail.com>
2024-09-27 21:59:51 +0200

    Write combined sequence test

--- compress.test.js
+++ compress.test.js
@@ -31,11 +31,11 @@ describe('compress function', () => {
     expect(result).toBe('2-8/2,10,13,14');
   });
 
-  it('should compress the string', () => {
+  it('should compress the string with multiple different sequences', () => {
     const compress = require('./compress.js').compress;
 
-    const result = compress([1, 1, 1, 2, 5, 6, 9, 10]);
+    const result = compress([1, 1, 2, 4, 6, 9, 8, 7, 10, 7, 4, 1]);
 
-    expect(result).toBe('1*3,2,5,6,9,10');
+    expect(result).toBe('1*2,2-6/2,5,6,9-7,10-1/3');
   });
 })
