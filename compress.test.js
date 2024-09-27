b9378e7fc4b40b429e15c31c290aef83cbed1774
Olaf <olaf.chuszno@gmail.com>
2024-09-27 17:48:21 +0200

    Write basic tests for compress function

---
+++ compress.test.js
@@ -0,0 +1,41 @@
+describe('compress function', () => {
+  it('should compress duplicates', () => {
+    const compress = require('./compress.js').compress;
+
+    const result = compress([1, 1, 1, 2, 5, 6, 9, 10]);
+
+    expect(result).toBe('1*3,2,5,6,9,10');
+  });
+
+  it('should compress consecutive values', () => {
+    const compress = require('./compress.js').compress;
+
+    const result = compress([1, 2, 3, 4, 5, 8, 20, 21, 25]);
+
+    expect(result).toBe('1-5,8,20,21,25');
+  });
+
+  it('should compress reversed consecutive values', () => {
+    const compress = require('./compress.js').compress;
+
+    const result = compress([5, 4, 3, 2, 1, 8, 20, 21, 25]);
+
+    expect(result).toBe('5-1,8,20,21,25');
+  });
+
+  it('should compress interval values', () => {
+    const compress = require('./compress.js').compress;
+
+    const result = compress([2, 4, 6, 8, 10, 13, 14]);
+
+    expect(result).toBe('2-8/2,10,13,14');
+  });
+
+  it('should compress the string', () => {
+    const compress = require('./compress.js').compress;
+
+    const result = compress([1, 1, 1, 2, 5, 6, 9, 10]);
+
+    expect(result).toBe('1*3,2,5,6,9,10');
+  });
+})
